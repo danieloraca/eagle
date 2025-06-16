1f956381672e6ca1b302094f91d9fd2a1d4009ba
Dan <daniel.oraca@gmail.com>
2025-06-16 20:24:29 +0100

    fixes

--- src/space_objects.rs
+++ src/space_objects.rs
@@ -1,12 +1,12 @@
 pub struct Star {
-    x: f32,
-    y: f32,
-    z: f32,
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
 }
 
 pub struct BigStar {
-    x: f32,
-    y: f32,
-    z: f32,
-    hit: bool,
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub hit: bool,
 }
