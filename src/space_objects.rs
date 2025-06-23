cceff906b5b938219aab247af4871f1de87f2925
Dan <daniel.oraca@gmail.com>
2025-06-23 20:06:13 +0100

    more tests

--- src/space_objects.rs
+++ src/space_objects.rs
@@ -21,3 +21,64 @@ impl BigStar {
         sx < 0.0 || sx > width as f32 || sy < 0.0 || sy > height as f32
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_star_center_on_screen() {
+        let width = 800;
+        let height = 600;
+        let star = BigStar {
+            x: 0.0,
+            y: 0.0,
+            z: 1.0,
+            hit: false,
+            was_missed: false,
+        };
+        assert_eq!(star.is_off_screen(width, height), false);
+    }
+
+    #[test]
+    fn test_star_far_left_off_screen() {
+        let width = 800;
+        let height = 600;
+        let star = BigStar {
+            x: -20.0,
+            y: 0.0,
+            z: 10.0,
+            hit: false,
+            was_missed: false,
+        };
+        assert_eq!(star.is_off_screen(width, height), true);
+    }
+
+    #[test]
+    fn test_star_far_right_off_screen() {
+        let width = 800;
+        let height = 600;
+        let star = BigStar {
+            x: 10.0,
+            y: 0.0,
+            z: 1.0,
+            hit: false,
+            was_missed: false,
+        };
+        assert_eq!(star.is_off_screen(width, height), true);
+    }
+
+    #[test]
+    fn test_star_below_screen() {
+        let width = 800;
+        let height = 600;
+        let star = BigStar {
+            x: 0.0,
+            y: 10.0,
+            z: 1.0,
+            hit: false,
+            was_missed: false,
+        };
+        assert_eq!(star.is_off_screen(width, height), true);
+    }
+}
